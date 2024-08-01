d4538685eabd55710f387eae9b009513791afc43
Jadoson <ghost1342@gmail.com>
2024-08-01 14:22:37 +0300

    Added add category button

--- src/pages/CategoriesPage.vue
+++ src/pages/CategoriesPage.vue
@@ -1,6 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CategoryList from 'src/components/CategoryList.vue';
+import CategoryAdd from 'src/components/CategoryAdd.vue';
+</script>
 <template>
   <q-page class="column items-center justify-center">
     <h1 class="text-h1">Categories Page</h1>
+    <category-list />
+    <category-add />
   </q-page>
 </template>
