9f27df34d158ad7932a06e3b1c2427774e237970
Jadoson <ghost1342@gmail.com>
2024-08-01 15:36:17 +0300

    Added add expense button

--- src/pages/ExpensesPage.vue
+++ src/pages/ExpensesPage.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ExpenseAdd from 'src/components/ExpenseAdd.vue';
+</script>
 <template>
   <q-page class="column items-center justify-center">
     <h1 class="text-h1">Expenses page</h1>
+    <expense-add />
   </q-page>
 </template>
