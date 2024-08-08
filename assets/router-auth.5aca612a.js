4f4c088d64571c3971e6e7afe1be25deaa52071b
Jadoson <ghost1342@gmail.com>
2024-08-08 21:14:09 +0300

    Updates

--- assets/router-auth.5aca612a.js
+++
@@ -1,1 +0,0 @@
-import{b as s,s as u}from"./index.133f7485.js";var f=s(({router:a})=>{a.beforeEach(e=>{if(e.matched.some(t=>{var r;return(r=t.meta.requiresAuth)!=null?r:!1})&&!u.value)return{name:"auth",query:{redirect:e.fullPath}}})});export{f as default};
